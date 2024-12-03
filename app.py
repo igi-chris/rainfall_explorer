7d707550f52440e3b4e8d5ea794749891da0e602
igi-chris <62021095+igi-chris@users.noreply.github.com>
2024-12-03 17:38:49 +0000

    expose server

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from callbacks import register_callbacks
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = app_layout
 register_callbacks(app)
+server = app.server
 
 if __name__ == "__main__":
     app.run_server(debug=True)
